b38a1b22380e17a91cbf003f42bd43364e264e9c
El Luco <saintsuperylucas@gmail.com>
2025-03-29 22:03:37 +0100

    icov.info

---
+++ mateleau-crm-frontend/karma.conf.js
@@ -0,0 +1,12 @@
+module.exports = function (config) {
+    config.set({
+      reporters: ['progress', 'coverage'],
+      coverageReporter: {
+        type: 'lcov',
+        dir: 'coverage/',
+        subdir: '.',
+        file: 'lcov.info'
+      }
+    });
+  };
+  
