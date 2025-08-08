35889f007f5a3f0a0023ddd1d062d5b1ee923e5b
El Luco <saintsuperylucas@gmail.com>
2025-08-08 18:37:44 +0200

    feat(env) ajout de l'environement de production

---
+++ mateleau-crm-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://crm-mateleau-isvbf.ondigitalocean.app/api/api/',
+    apiAuth: 'https://crm-mateleau-isvbf.ondigitalocean.app/api/api/auth/',
+    apiUsers: 'https://crm-mateleau-isvbf.ondigitalocean.app/api/api/users/',
+    apiDives: 'https://crm-mateleau-isvbf.ondigitalocean.app/api/api/dives/',
+    apiBoats: 'https://crm-mateleau-isvbf.ondigitalocean.app/api/api/boats/',
+    apiAviability: 'https://crm-mateleau-isvbf.ondigitalocean.app/api/api/availability/',
+    apiDiver: 'https://crm-mateleau-isvbf.ondigitalocean.app/api/api/divers/',
+    apiEquipment: 'https://crm-mateleau-isvbf.ondigitalocean.app/api/api/equipment/',
+    apiDivingGroup: 'https://crm-mateleau-isvbf.ondigitalocean.app/api/api/diving-groups/',
+    apiDashboard: 'https://crm-mateleau-isvbf.ondigitalocean.app/api/api/dashboard/',
+    apiTasks: 'https://crm-mateleau-isvbf.ondigitalocean.app/api/api/tasks/',
+    apiSpots: 'https://crm-mateleau-isvbf.ondigitalocean.app/api/api/spots/',
+  };
+  
+
+  
