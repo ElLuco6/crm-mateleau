38c67e2c0f003efebe7cdaecabc55532f022eef9
El Luco <saintsuperylucas@gmail.com>
2025-08-09 11:57:17 +0200

    fix: karma config pour avoir le bon rapport

--- mateleau-crm-frontend/karma.conf.js
+++ mateleau-crm-frontend/karma.conf.js
@@ -1,12 +1,22 @@
 module.exports = function (config) {
-    config.set({
-      reporters: ['progress', 'coverage'],
-      coverageReporter: {
-        type: 'lcov',
-        dir: 'coverage/',
-        subdir: '.',
-        file: 'lcov.info'
-      }
-    });
-  };
-  
+  config.set({
+    // ...
+    reporters: ['progress', 'kjhtml', 'coverage'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+    ],
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      reporters: [
+        { type: 'lcovonly', subdir: '.' },
+        { type: 'text-summary' }
+      ],
+      fixWebpackSourcePaths: true,
+    },
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+  });
+};
